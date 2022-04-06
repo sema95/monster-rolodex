147d0164b18afa667dced653c622a3b046f42e04
Max Sevenig <max.sevenig@hotmail.com>
2022-04-06 14:30:01 +0200

    Completed monster rolodex for udemy react project

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card-list.styles.css';
+import Card from '../card/card.component';
+
+const CardList = ({ monsters }) => (
+     <div className='card-list'>
+      {monsters.map((monster) => {
+        return (
+          <Card monster={monster}/>
+      )})}
+    </div>
+  );
+
+export default CardList;
